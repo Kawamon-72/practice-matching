057072e938f0b4c42c329860bc8b9e49ded5babd
川面諒羽 <tchimo21@gmail.com>
2025-06-01 16:01:35 +0900

    利用規約、プライポリシーの実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,13 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   before_action :require_login
+  skip_before_action :require_login, if: :high_voltage_page?
 
   private
+
+  def high_voltage_page?
+    controller_path == 'high_voltage/pages'
+  end
 
   def not_authenticated
     redirect_to login_path
